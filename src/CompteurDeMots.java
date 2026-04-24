fd46151946ca222e1c73872d9762ee063e978560
bi tia <tiajoce7@gmail.com>
2026-04-24 20:53:37 +0000

    Ajout

--- src/CompteurDeMots.java
+++ src/CompteurDeMots.java
@@ -5,22 +5,21 @@ import java.util.Scanner;
 public class CompteurDeMots {
     public static void main(String[] args) {
         try {
-            // Le fichier "texte.txt" doit être dans le dossier du projet
+
             File file = new File("texte.txt");
             Scanner scanner = new Scanner(file);
 
-            int totalMots = 0;
+            int total = 0;
 
-            // Boucle qui lit chaque ligne du fichier
+
             while (scanner.hasNextLine()) {
                 String ligne = scanner.nextLine();
-                // Sépare la ligne en mots en utilisant l'espace comme délimiteur
                 String[] mots = ligne.split(" ");
-                totalMots += mots.length;
+                total += mots.length;
             }
 
             scanner.close();
-            System.out.println("Le nombre total de mots dans ce fichier est : " + totalMots);
+            System.out.println("Le total des mots est de : " + total);
 
         } catch (FileNotFoundException e) {
             System.out.println("Fichier introuvable !");
